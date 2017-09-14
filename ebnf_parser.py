b4c8286c6d5736c072a4e991480ac6d9222c3791
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2017-09-13 22:03:27 -0700

    Added some match objects

--- ebnf_parser.py
+++ ebnf_parser.py
@@ -15,6 +15,28 @@ class Parser(object):
 		print("Matching text")
 		print(text)
 
+class MatchNode(object):
+	def __init__(self, start, end, children):
+		self.start = start
+		self.end = end
+		self.children = children
+
+class TextMatch(MatchNode):
+	def __init__(self, start, end, children):
+		super(TextMatch, self).__init__(start, end, children)
+
+class RepeatMatch(MatchNode):
+	def __init__(self, start, end, children, num_repeats):
+		super(RepeatMatch, self).__init__(start, end, children)
+		self.num_repeats = num_repeats
+
+class RepeatMatch(MatchNode):
+	def __init__(self, start, end, children, num_repeats):
+		super(RepeatMatch, self).__init__(start, end, children)
+		self.num_repeats = num_repeats
+
+
+
 class BaseTerm(object):
 	def __init__(self, rule_string):
 		self.rule_string = rule_string
@@ -25,14 +47,16 @@ class BaseTerm(object):
 class TextTerm(BaseTerm):
 	def __init__(self, rule_string):
 		super(TextTerm, self).__init__(rule_string)
-		self.possibles = possibles
 	
 class FullTerm(BaseTerm):
 	def __init__(self, rule_string, possibles):
 		super(FullTerm, self).__init__(rule_string)
 		self.possibles = possibles
 
-class RepeatTerm(BaseTerm):
+class RepeatTerm(FullTerm):
 	def __init__(self, rule_string, possibles):
 		super(RepeatTerm, self).__init__(rule_string, possibles)
 		
+class VariableTerm(FullTerm): # TODO: Make sure we keep only one instance per variable
+	def __init__(self, rule_string, possibles):
+		super(VariableTerm, self).__init__(rule_string, possibles)
