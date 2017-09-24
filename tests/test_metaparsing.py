45c2083b0dc624aea1b23d12eb8e03bfaeaebb38
Aryan Falahatpisheh <aryan.falahatpisheh@berkeley.edu>
2017-09-24 12:20:17 -0700

    Added simple metaparsing plan

---
+++ tests/test_metaparsing.py
@@ -0,0 +1,12 @@
+import unittest
+import ebnf_parser
+
+class TestMetaparsing(unittest.TestCase):
+
+
+    def test_text_term(self):
+        raw_text = "\"abc\""
+
+
+if __name__ == '__main__':
+    unittest.main()
